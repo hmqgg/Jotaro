5147754d04780c987c4a84c0ec5d542e79d222c8
Mitch Hwang <mitchx7@outlook.com>
2022-12-14 07:08:22 +0800

    feat: startup sqlite migration

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -1,6 +1,20 @@
+use diesel::{prelude::*, r2d2::{ConnectionManager, Pool, PooledConnection}};
 use serenity::{prelude::*, model::prelude::*, async_trait};
 
-pub struct Handler;
+pub struct Handler {
+    pool: Pool<ConnectionManager<SqliteConnection>>,
+}
+
+impl Handler {
+    pub fn new(p: Pool<ConnectionManager<SqliteConnection>>) -> Self {
+        Self { pool: p }
+    }
+
+    #[allow(dead_code)]
+    fn conn(&self) -> PooledConnection<ConnectionManager<diesel::SqliteConnection>> {
+        self.pool.get().unwrap()
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+use diesel::{r2d2::{ConnectionManager, self}, SqliteConnection};
+use diesel_migrations::{EmbeddedMigrations, embed_migrations, MigrationHarness};
 use dotenvy::dotenv;
 use jotaro::handler::Handler;
 use serenity::{Client, prelude::*};
 use std::env;
 
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
+    let manager = ConnectionManager::<SqliteConnection>::new(
+        env::var("DATABASE_URL").expect("Expected a database url in the environment"));
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+    let conn = &mut *pool.get().unwrap();
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
+
+    let handler = Handler::new(pool);
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let mut client = Client::builder(&token, GatewayIntents::empty())
-        .event_handler(Handler)
+        .event_handler(handler)
         .await
         .expect("Error creating client");
 
